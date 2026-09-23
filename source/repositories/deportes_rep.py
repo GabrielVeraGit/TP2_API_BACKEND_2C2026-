d28ce8217aa37b02015723ee9b8d23475d8bdc98
Agustin Panozzo <apanozzo@fi.uba.ar>
2026-09-23 00:56:44 -0300

    funcionalidad ruta deportes

--- source/repositories/deportes_rep.py
+++ source/repositories/deportes_rep.py
@@ -1,1 +1,10 @@
 from source.db import ejecutar_instruccion
+
+
+def obtener_deportes_rep():
+    query = """
+        SELECT id, nombre
+        FROM deportes
+    """
+
+    return ejecutar_instruccion(query)
--- source/routes/deportes_rou.py
+++ source/routes/deportes_rou.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
+from source.repositories.deportes_rep import obtener_deportes_rep
 
 deportes_bp = Blueprint("deportes",__name__)
 
 @deportes_bp.route("/deportes", methods=["GET"])
 def obtener_deportes():
-    print("obtener deportes")
-    return "retornar deportes"
+
+    resultados = obtener_deportes_rep()
+
+    if not resultados:
+        return {"message": "No se encontraron deportes."}, 204
+
+    return resultados, 200
